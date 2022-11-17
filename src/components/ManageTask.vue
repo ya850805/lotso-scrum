bf6ce5b990ac806f444cfda936c8b138aec03da4
Jason <ya850805@gmail.com>
2022-11-18 00:50:56 +0800

    feat: Add stable story points and initial scrum tasks.

--- src/components/ManageTask.vue
+++ src/components/ManageTask.vue
@@ -2,17 +2,27 @@
 <template>
   請把需求放到產品待辦清單，並調整待辦的優先度順序。
   我們公司也推薦使用 Jira 來做任務的管理呢！
-  <RouterLink to="/scrum-intro" @click="$emit('setProgressRate', 30)">Next</RouterLink>
+
+
+  <RouterLink to="/scrum-intro" @click="$emit('setProgressRate', 30); refreshLocalStorage()">Submit</RouterLink>
 </template>
 
 <script setup>
-import {onMounted} from "vue";
-import {TASKS_KEY} from "@/constant/const";
+import {onMounted, ref} from "vue";
+import {INIT_SCRUM_TASK, TASKS_KEY} from "@/constant/const";
+
+let taskArray;
 
 onMounted(() => {
-  const taskArray = []
-  localStorage.setItem(TASKS_KEY, JSON.stringify(taskArray))
+  taskArray = ref(INIT_SCRUM_TASK)
 })
+
+//TODO manage tasks - Add, Delete, Edit
+
+function refreshLocalStorage() {
+  localStorage.removeItem(TASKS_KEY)
+  localStorage.setItem(TASKS_KEY, JSON.stringify(taskArray.value))
+}
 </script>
 
 <style scoped>
--- src/constant/const.ts
+++ src/constant/const.ts
@@ -1,1 +1,30 @@
 export const TASKS_KEY = "lotso-scrum-tasks"
+
+export const TASK_STORY_POINTS = [1, 2, 3, 5, 8, 13, 21]
+
+export const INIT_SCRUM_TASK = [
+    {
+        id: 1,
+        name: "會員系統(登入、註冊、權限管理)",
+        link: "https://www.atlassian.com/software/jira",
+        points: null
+    },
+    {
+        id: 2,
+        name: "前台職缺列表、應徵",
+        link: "https://www.atlassian.com/software/jira",
+        points: TASK_STORY_POINTS[3]
+    },
+    {
+        id: 3,
+        name: "應徵者的線上履歷編輯器",
+        link: "https://www.atlassian.com/software/jira",
+        points: TASK_STORY_POINTS[4]
+    },
+    {
+        id: 4,
+        name: "後台職缺管理功能(資訊上架、下架、顯示資料)",
+        link: "https://www.atlassian.com/software/jira",
+        points: TASK_STORY_POINTS[6]
+    },
+]
