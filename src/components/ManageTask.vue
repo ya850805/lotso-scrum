ffe29c6f57c42e2142aa37cfdad7236a2bd44f96
Jason <ya850805@gmail.com>
2022-11-17 21:38:27 +0800

    feat: Add ManageTask(page2) component.

---
+++ src/components/ManageTask.vue
@@ -0,0 +1,13 @@
+<!--page2-->
+<template>
+  請把需求放到產品待辦清單，並調整待辦的優先度順序。
+  我們公司也推薦使用 Jira 來做任務的管理呢！
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Greeting from '../components/Greeting.vue'
 import POIntro from '../components/POIntro.vue'
+import ManageTask from '../components/ManageTask.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'po-intro',
       component: POIntro
     },
+    {
+      path: '/manage-task',
+      name: 'manage-task',
+      component: ManageTask
+    }
   ]
 })
 
