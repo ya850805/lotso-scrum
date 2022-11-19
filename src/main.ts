ca4acc51ad21b0d4ab66b8abc00c4da427bc5c8b
Jason <ya850805@gmail.com>
2022-11-19 13:36:26 +0800

    docs: remove todo.

--- src/main.ts
+++ src/main.ts
@@ -15,4 +15,3 @@ app.component('draggable', draggable)
 
 //TODO use template with slot
 //TODO when every component mounted, need to reset progress bar.
-//TODO fix issue that refresh router link will cause 404 in github pages.
